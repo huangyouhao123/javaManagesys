1e222d5b344ca6aff1562006a2d0e7fa924c37a9
huangyouhao123 <1592185893@qq.com>
2026-01-11 22:46:08 +0800

    addcode

---
+++ telisa-web-management/src/main/java/com/freshfish/telisawebmanagement/TelisaWebManagementApplication.java
@@ -0,0 +1,13 @@
+package com.freshfish.telisawebmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelisaWebManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelisaWebManagementApplication.class, args);
+    }
+
+}
---
+++ telisa-web-management/src/test/java/com/freshfish/telisawebmanagement/TelisaWebManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshfish.telisawebmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelisaWebManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
